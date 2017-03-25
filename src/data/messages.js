c20f2a633900b8edd6e3415cd47e9df1fa549fe3
Onaracs <brendan.scarano@gmail.com>
2017-03-25 12:14:18 -0400

    added more messages

--- src/data/messages.js
+++ src/data/messages.js
@@ -2,14 +2,18 @@ const possibleYesBlocks = [
   {text: 'Thanks', emoji: '+1'},
   {text: 'Eye appreciate that', emoji: 'eye'},
   {text: 'This contact will see another day', emoji: 'sunrise'},
+  {text: 'Grazie', emoji: 'flag-it'},
+  {text: 'Merci', emoji: 'flag-fr'},
 ]
 
 const possibleDeniedBlocks = [
   {text: 'Goodbye', emoji: 'wave'},
   {text: 'Addio', emoji: 'flag-it'},
   {text: 'Au Revoir', emoji: 'flag-fr'},
-  {text: 'Hasta la vista, Baby', emoji: 'robot-face'},
+  {text: 'Hasta la vista, Baby', emoji: 'robot'},
   {text: 'So long, King Bowser!', emoji: 'mushroom'},
+  {text: 'Say goodnight to the bad guy', emoji: 'gun'},
+  {text: 'Long live, the king!', emoji: 'lion'},
 ]
 
 export {
